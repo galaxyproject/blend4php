7cb4f9929a4e21ea25e8cdf34b697d6a0823d99b
Brian <kungfunub@gmail.com>
2015-09-29 16:55:48 -0700

    Added some stuff to the tool class

--- src/TestUser.php
+++ src/TestUser.php
@@ -2,19 +2,21 @@
 include 'UserClient.inc';
 include 'GalaxyInstance.inc';
 include 'WorkflowClient.inc';
-
-
+include 'ToolClient.inc';
 
 
  $galaxy = new GalaxyInstance('localhost', '8080');
  $galaxy-> authenticate('brian@yahoo.com', 'password');
  $user= new UserClient($galaxy);
  $workflow = new WorkflowClient($galaxy);
- 
+ $tool = new ToolClient($galaxy);
  
  print 'I have started the test';
 
- $workflow_id = $workflow->obtainWorkflow_id('connor');
- print $workflow->create(NULL, NULL, NULL, NULL, NULL, NULL, "5969b1f7201f12ae", NULL, NULL, NULL, 'I hope this works');
+// $workflow_id = $workflow->obtainWorkflow_id('connor');
+// print $workflow->create(NULL, NULL, NULL, NULL, NULL, NULL, "5969b1f7201f12ae", NULL, NULL, NULL, 'I hope this works');
+$tool_id = $tool->obtainTool_id("HbVar");
+print $tool_id;
+//print $tool->get_tools($tool_id); 
 
 ?>
