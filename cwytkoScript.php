a6caf8546b9fb41ff7537a8151e033b4cc5b42e3
brotato <cgpwytko@gmail.com>
2015-10-07 12:35:44 -0700

    I want to pull Brian's changes

--- cwytkoScript.php
+++ cwytkoScript.php
@@ -19,11 +19,15 @@ $galaxy->authenticate('cgpwytko@gmail.com', 'potato15');
 $wfc = new WorkflowClient($galaxy);
 
 //$wfc->delete_workflow('1cd8e2f6b131e891');
-//f597429621d6eb2b ebfb8f50c6abde6d
-$wfc->export_workflow_json('f597429621d6eb2b');
+//f597429621d6eb2b ebfb8f50c6abde6d 33b43b4e7093c91f
+//$wfc->export_workflow_json('5969b1f7201f12ae');
+//$wfc -> index();
+
 
 //$wfc -> export_workflow_to_local_path('ebfb8f50c6abde6d', '/home/cwytko/Downloads');
  //$wfc -> get_workflow_inputs('f597429621d6eb2b', 'Reference annotati');
  //$wfc -> get_workflows(NULL, 'potato');
  
-$wfc -> import_shared_workflow();
+//$wfc -> import_shared_workflow();
+
+$wfc -> import_workflow_from_local_path ('/home/cwytko/Downloads/Galaxy-Workflow-RNAseqTRAPLINE.ga');
