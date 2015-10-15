5b3dab8047d318f4c9128f8dfc4b515eb8744b4b
Brian <kungfunub@gmail.com>
2015-10-15 14:54:12 -0700

    Added some functions to toolclient.

--- src/TestUser.php
+++ src/TestUser.php
@@ -11,13 +11,17 @@ include 'ToolClient.inc';
  $workflow = new WorkflowClient($galaxy);
  $tool = new ToolClient($galaxy);
  
- print 'I have started the test';
+ print 'I have started the test ';
 
- print $tool->executeTool("sort1", "1.0.3", "files__UCSC_Main_on_Human__knownGene_(genome).bed");
+
 // $workflow_id = $workflow->obtainWorkflow_id('connor');
 // print $workflow->create(NULL, NULL, NULL, NULL, NULL, NULL, "5969b1f7201f12ae", NULL, NULL, NULL, 'I hope this works');
 //$tool_id = $tool->obtainTool_id("HbVar");
 //print $tool_id;
-print $tool->get_tools(); 
+//print $tool->get_tools(); 
+//print $tool->executeTool('genomespace_file_browser_prod', '1.01');
+//print $tool->build_tool('genomespace_file_browser_prod');
+//print $tool->diagnostics('genomespace_file_browser_prod');
+print $tool->reload('genomespace_file_browser_prod');
 
 ?>
