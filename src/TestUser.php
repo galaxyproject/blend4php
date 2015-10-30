2d520722c529fa41f9e675c53aca6e1470d98307
Brian <kungfunub@gmail.com>
2015-10-29 17:06:01 -0700

    I have updated some files yay!!

--- src/TestUser.php
+++ src/TestUser.php
@@ -1,21 +1,22 @@
 <?php
-include 'UserClient.inc';
+include 'Users.inc';
 include 'GalaxyInstance.inc';
-include 'WorkflowClient.inc';
-include 'ToolClient.inc';
+include 'Workflows.inc';
+include 'Tools.inc';
 include 'Histories.inc';
 
 
  $galaxy = new GalaxyInstance('localhost', '8080');
  $galaxy-> authenticate('brian@yahoo.com', 'password');
- $user= new UserClient($galaxy);
- $workflow = new WorkflowClient($galaxy);
- $tool = new ToolClient($galaxy);
+ $user= new Users($galaxy);
+ $workflow = new Workflows($galaxy);
+ $tool = new Tools($galaxy);
  $history = new Histories($galaxy);
  
  print 'I have started the test ';
 
+ print $tool->build_tool('Interval2Maf_pairwise1');
 //print $workflow->get_workflows();
 //print $history->archive_export("5969b1f7201f12ae");
-print $history->deleteHistory('df7a1f0c02a5b08e');
+//print $history->deleteHistory('df7a1f0c02a5b08e');
 ?>
