cf6b976d1aa2c6be998af7ac767b26627331a3b3
Brian <kungfunub@gmail.com>
2015-11-03 16:53:34 -0800

    I have done some stuff but I forgot what I completed...

--- src/TestUser.php
+++ src/TestUser.php
@@ -3,20 +3,23 @@
 include 'GalaxyInstance.inc';
 //include 'Workflows.inc';
 include 'Tools.inc';
+include 'Histories.inc';
+include 'HistoryContents.inc';
 
 
 
  $galaxy = new GalaxyInstance('localhost', '8080');
  $galaxy-> authenticate('brian@yahoo.com', 'password');
+ $historyContent = new HistoryContents($galaxy); 
  //$user= new Users($galaxy);
  //$workflow = new Workflows($galaxy);
- $tool = new Tools($galaxy);
- //$history = new Histories($galaxy);
- 
- print 'I have started the test ';
+ //$tool = new Tools($galaxy);
 
- print $tool->build_tool('Intervalfeaw2Maf_pairwise1');
- print $tool->getErrorMessage();
+print $historyContent->create('5969b1f7201f12ae');
+print $historyContent->getErrorMessage();
+
+ //print $tool->build_tool('Intervalfeaw2Maf_pairwise1');
+ //print $tool->getErrorMessage();
 //print $workflow->get_workflows();
 //print $history->archive_export("5969b1f7201f12ae");
 //print $history->deleteHistory('df7a1f0c02a5b08e');
