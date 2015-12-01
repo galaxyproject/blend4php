a7be040c97b1521d90adc96e9263787d969ece67
Brian <kungfunub@gmail.com>
2015-12-01 13:38:30 -0800

    I have done some work on tools. YAY!

--- src/TestUser.php
+++ src/TestUser.php
@@ -3,18 +3,23 @@ include 'Users.inc';
 include_once 'GalaxyInstance.inc';
 //include_once 'Workflows.inc';
 include_once 'DataTypes.inc';
-//include_once 'Tools.inc';
+include_once 'Tools.inc';
 include_once 'Histories.inc';
 //include_once 'HistoryContents.inc';
 
 
 
  $galaxy = new GalaxyInstance('localhost', '8080');
- $galaxy-> authenticate('bob@gmail.com', 'password');
- $datatype = new Datatypes($galaxy);
+ $galaxy-> authenticate('brian@yahoo.com', 'password');
+ $tool = new Tools($galaxy);
+ 
+ //print $tool->download("ucsc_table_direct_archaea1");
+ print $tool->index("ucsc_table_direct_archaea1", NULL, TRUE)
+ 
+ //$datatype = new Datatypes($galaxy);
  //print $datatype->edam_formats();
- $history = new Histories($galaxy);
-print $history->index(); 
+ //$history = new Histories($galaxy);
+//print $history->index(); 
 
 
 ?>
