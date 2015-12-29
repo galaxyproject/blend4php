0fde78271a678d2f1bdbe25b61c5a1f3e1feee0d
kungfunub <kungfunub@gmail.com>
2015-12-28 21:00:52 -0800

    I have made a winte break commit! wooha Users has been updated

--- src/TestUser.php
+++ src/TestUser.php
@@ -7,29 +7,23 @@ include_once 'DataTypes.inc';
 include_once 'Tools.inc';
 include_once 'Histories.inc';
 include_once 'HistoryContents.inc';
-
+include_once 'Users.inc';
 
 
  $galaxy = new GalaxyInstance('localhost', '8080');
- $galaxy-> authenticate('brian@yahoo.com', 'password');
- $tool = new Tools($galaxy);
- $roles = new Roles($galaxy);
- $historyContents = new HistoryContents($galaxy);
- //print $historyContents ->update("5969b1f7201f12ae", '5969b1f7201f12ae', array('name'=>'booba'));
- //print $historyContents->delete('5969b1f7201f12ae', '5969b1f7201f12ae', True);
- print $historyContents->index('5969b1f7201f12ae',"5969b1f7201f12ae,a799d38679e985db" );
- print "! ! !! The error message is: ". $historyContents -> getErrorMessage() . " ";
- //print $roles->create('Make Admin out of you 12', 'I am testing how this role stuff works', array('df7a1f0c02a5b08e', '1cd8e2f6b131e891'));
- //print "the error report: " . $roles->getErrorMessage();
- //print $roles->index();
- //print $roles->show('f2db41e1fa331b3e');
- //print $tool->download("ucsc_table_direct_archaea1");
- //print $tool->index("ucsc_table_direct_archaea1", NULL, TRUE)
- //print $tool->build("ucsc_table_direct_archaea1");
- //$datatype = new Datatypes($galaxy);
- //print $datatype->edam_formats();
- //$history = new Histories($galaxy);
-//print $history->index(); 
+ $galaxy-> authenticate('brian@gmail.com', 'password');
+ $users = new Users($galaxy);
+ //print $users->create("blarb", "blarb@gmail.com", "blarby");
+ //print $users->api_key("f597429621d6eb2b");
+ //print $users->delete("f597429621d6eb2b");
+ //print $users->index();
+ 
+ 
+ print $users->index(true,"blarb@gmail.com",'bri',true);
+ print $users->getErrorMessage();
 
+ 
+
+ 
 
 ?>
