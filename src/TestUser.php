1e11c728c28f17c82c2e37938530e523ea47c58f
Brian <kungfunub@gmail.com>
2016-01-25 12:47:23 -0800

    Got Search To work

--- src/TestUser.php
+++ src/TestUser.php
@@ -8,26 +8,23 @@ include_once 'Tools.inc';
 include_once 'Histories.inc';
 include_once 'HistoryContents.inc';
 include_once 'Users.inc';
+include_once 'Search.inc';
 
 
  $galaxy = new GalaxyInstance('localhost', '8080');
  $galaxy->authenticate('brian@yahoo.com', 'password');
  
  $workflow = new Workflows($galaxy);	
-
+ $search = new Search($galaxy);
 
 
 
  
+ //print $search->create('SELECT * FROM HISTORIES WHERE ID = 290670ee50ab85f0');
+ print $search -> create('select * from history where id = \'290670ee50ab85f0\'');
+ print $search->getErrorMessage();
  
- //print $users->index(true,"blarb@gmail.com",'bri',true);
- //print $users->getErrorMessage();
- print $workflow->invoke2('a799d38679e985db');
- //print $workflow->invoke('f597429621d6eb2b');
- print $workflow->getErrorMessage();
-
- 
-
+ //'select'. trail: [expr]
 
  
 
