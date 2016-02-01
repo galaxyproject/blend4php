a5df4d61119c7fb5c10c893da98324c005dac2c0
theBrian <kungfunub@gmail.com>
2016-01-31 20:50:09 -0800

    Had an issue with my laptop, I hope everything here is right

--- src/TestUser.php
+++ src/TestUser.php
@@ -10,17 +10,30 @@ include_once 'HistoryContents.inc';
 include_once 'Users.inc';
 include_once 'Search.inc';
 include_once 'Visualizations.inc';
+include_once 'Jobs.inc';
+include_once 'Groups.inc';
+
 
 
  $galaxy = new GalaxyInstance('localhost', '8080');
- $galaxy->authenticate('brian@yahoo.com', 'password');
+ $galaxy->authenticate('bob@gmail.com', 'password');
+ $user = new Users($galaxy);
+ //print $user->create('blarb', 'blarb@gmail.com', 'password');
+ $role = new Roles($galaxy);
+ //print $role->create('Teddy Bear Hugger3', 'Hugs teddy bears', array('f597429621d6eb2b'));
+ //print $role->getErrorMessage();
+ $group = new Groups($galaxy);
+ //print $group->create('pedophilia2 attack of the bear', array('f597429621d6eb2b'), array('f2db41e1fa331b3e'));
+ print $group->update('f2db41e1fa331b3e', 'name changed to this');
+ //$Job = new Jobs($galaxy);
+ //print $Job->search('sort1',array('a676e8f07209a3be'));
+ //$workflow = new Workflows($galaxy);
  
- $workflow = new Workflows($galaxy);	
- $search = new Search($galaxy);
-$visualization = new Visualizations($galaxy);
+ //$search = new Search($galaxy);
+//$visualization = new Visualizations($galaxy);
 
-print $visualization->create('blob3','it worked again!','00112');
-print $visualization->getErrorMessage();
+//print $visualization->create('blob3','it worked again!','00112');
+//print $visualization->getErrorMessage();
 
 
  
