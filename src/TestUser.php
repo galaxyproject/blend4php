78fb36494990caa948bb39023d09fbcb2da5ea7a
Brian <kungfunub@gmail.com>
2016-02-01 15:50:00 -0800

    Made updates to history contents

--- src/TestUser.php
+++ src/TestUser.php
@@ -16,26 +16,8 @@ include_once 'Groups.inc';
 
 
  $galaxy = new GalaxyInstance('localhost', '8080');
- $galaxy->authenticate('bob@gmail.com', 'password');
- $user = new Users($galaxy);
- //print $user->create('blarb', 'blarb@gmail.com', 'password');
- $role = new Roles($galaxy);
- //print $role->create('Teddy Bear Hugger3', 'Hugs teddy bears', array('f597429621d6eb2b'));
- //print $role->getErrorMessage();
- $group = new Groups($galaxy);
- //print $group->create('pedophilia2 attack of the bear', array('f597429621d6eb2b'), array('f2db41e1fa331b3e'));
- print $group->update('f2db41e1fa331b3e', 'name changed to this');
- //$Job = new Jobs($galaxy);
- //print $Job->search('sort1',array('a676e8f07209a3be'));
- //$workflow = new Workflows($galaxy);
- 
- //$search = new Search($galaxy);
-//$visualization = new Visualizations($galaxy);
-
-//print $visualization->create('blob3','it worked again!','00112');
-//print $visualization->getErrorMessage();
-
-
- 
-
+ $galaxy->authenticate('brian@yahoo.com', 'password');
+ $history = new HistoryContents($galaxy);
+ print $history->create('290670ee50ab85f0', '6505e875ddb66fd2');
+ print $history->getErrorMessage();
 ?>
