138289650afaa266d019183bc333897544de4b0f
Brian <kungfunub@gmail.com>
2016-02-03 15:23:32 -0800

    Updated tools. Updated History contents

--- src/TestUser.php
+++ src/TestUser.php
@@ -18,6 +18,15 @@ include_once 'Groups.inc';
  $galaxy = new GalaxyInstance('localhost', '8080');
  $galaxy->authenticate('brian@yahoo.com', 'password');
  $history = new HistoryContents($galaxy);
- print $history->create('290670ee50ab85f0', '6505e875ddb66fd2');
+// print $history->create('290670ee50ab85f0', '6505e875ddb66fd2');
+ //print $history->update('290670ee50ab85f0', '33b43b4e7093c91f');
+
+ /*print $history->getErrorMessage();
+ print $history->delete('290670ee50ab85f0', '6505e875ddb66fd2');
  print $history->getErrorMessage();
+ */
+ $tool = new Tools($galaxy);
+ $inputs = array('33b43b4e7093c91f');
+ print $tool->build('sort1', $inputs);
+ print $tool->getErrorMessage();
 ?>
