609bff4a42bd0ca10d0ae756f67d5a52ac20ce55
Brian <kungfunub@gmail.com>
2016-02-12 14:38:52 -0800

    Updated Workflows

--- src/TestUser.php
+++ src/TestUser.php
@@ -18,9 +18,10 @@ include_once 'Groups.inc';
  $galaxy = new GalaxyInstance('localhost', '8080');
  $galaxy->authenticate('brian@yahoo.com', 'password');
 	$workflow = new Workflows($galaxy);
-	//print $workflow->update("a799d38679e985db");
-	print $workflow->build_module('6505e875ddb66fd2',array('8a81cf6f989c4467'));
+	//print $workflow->update("f2db41e1fa331b3e");
+	//print $workflow->build_module('6505e875ddb66fd2',array('8a81cf6f989c4467'));
 	//print $workflow->invoke('ebfb8f50c6abde6d', array('33b43b4e7093c91f','3cc0effd29705aa3'));
-	
+	//print $workflow->create('f2db41e1fa331b3e');
+	print $workflow->update_invocation_steps('f2db41e1fa331b3e','f2db41e1fa331b3e','f597429621d6eb2b');
 	print $workflow->getErrorMessage();
 ?>
