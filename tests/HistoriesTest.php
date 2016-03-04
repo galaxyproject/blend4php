76a78450ec79c97d6149fd818bf489567f7e2982
cwytko <cgpwytko@gmail.com>
2016-03-03 22:31:01 -0800

    not done yet but have improved the histories file and the HTTPRequest DOWNLOAD request method works properly now!

--- tests/HistoriesTest.php
+++ tests/HistoriesTest.php
@@ -16,7 +16,7 @@ class HistoriesTest extends PHPUnit_Framework_TestCase {
     $galaxy->authenticate($config['user'], $config['pass']);
     $hist = new Histories($galaxy);
 
-//    $hist->create('testhistorycreate');
+   $hist->create('testhistorycreate');
 
     $response = $hist->GET($config['host'] . ':' . $config['port'] . '/api/histories/?key=' . $config['api_key']);
 
@@ -83,17 +83,99 @@ class HistoriesTest extends PHPUnit_Framework_TestCase {
   }
 
   /**
+   * This function prepares a downloadable file a selected history.
+   *
+   * This test calls two functions from the Histories.inc:
+   * 1. archiveDownload
+   * 2. archiveExport
+   *
    * @depends testShow
    */
   public function testArchiveDownload($result) {
-
     global $config;
-
     $galaxy = new GalaxyInstance($config['host'], $config['port']);
     $galaxy->authenticate($config['user'], $config['pass']);
     $hist = new Histories($galaxy);
 
-    $response = $hist->archiveDownload($result['id']);
+    // We place it in /tmp as it's a temporary holding directory that any
+    // entity may place files
+    $response = $hist->archiveDownload($result['id'], "/tmp/phpUnitTestHistory.tar.gz");
 
+    $this->assertTrue($response);
+    $this->assertTrue(file_exists("/tmp/phpUnitTestHistory.tar.gz"));
+
+    return $result;
+  }
+
+  /**
+   * This function will 'delete' a history, hiding the history from the users.
+   *
+   * This action can be undone by undelete, this history can still be found by
+   * invoking the index() function
+   *
+   * @depends testArchiveDownload
+   */
+  public function testDeleteHistory($result){
+    global $config;
+    $galaxy = new GalaxyInstance($config['host'], $config['port']);
+    $galaxy->authenticate($config['user'], $config['pass']);
+    $hist = new Histories($galaxy);
+
+    $hist->deleteHistory($result['id']);
+
+    // Invoke the index() function, this will indirectly test the deleted parameter
+    // as well and if the particular id and whether the parameter of said id is
+    // deleted [as it's supposed to be from the call from the above call].
+    $hist->index();
+
+    $response = $hist->index();
+
+    // Now we check again to make sure the response is valid and we can
+    // find 'testhistorycreate'
+    $i = 0;
+    while (array_key_exists('id', $response[$i]) && array_key_exists('deleted',  $response[$i])) {
+
+      if ( $result['id'] == $response[$i]['id'] && $response[$i]['deleted'] == TRUE) {
+        break;
+      }
+      $i++ ;
+    }
+
+    // Once we break out we have found our candidate
+    $this->assertEquals($result['id'] ,$response[$i]['id']);
+    $this->assertTrue($response[$i]['deleted']);
+  }
+
+/**
+ *
+ *
+ * @depends testDeleteHistory
+ */
+  public function testUndelete($result){
+    global $config;
+    $galaxy = new GalaxyInstance($config['host'], $config['port']);
+    $galaxy->authenticate($config['user'], $config['pass']);
+    $hist = new Histories($galaxy);
+
+    var_dump($hist->undelete($result['id']));
+
+    // We run the index function again looking for two parameters again
+    // that the id we have undeletetd is back AND that the parameter
+    // deleted will be set to false
+    $response = $hist->index();
+
+   // var_dump($response);
+    $i = 0;
+    while (array_key_exists('id', $response[$i]) && array_key_exists('deleted',  $response[$i])) {
+
+      if ( $result['id'] == $response[$i]['id'] && $response[$i]['deleted'] == FALSE) {
+        break;
+      }
+      $i++ ;
+    }
+
+    // Once we break out we have found our candidate
+    $this->assertEquals($result['id'] ,$response[$i]['id']);
+    $this->assertFalse($response[$i]['deleted']);
   }
 }
