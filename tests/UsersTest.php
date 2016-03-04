3c72a50b6924e17f264ef2b01c56563e76844f54
theBrian <kungfunub@gmail.com>
2016-03-04 12:01:59 -0800

    Working on testusers.php

--- tests/UsersTest.php
+++ tests/UsersTest.php
@@ -4,7 +4,10 @@ require_once '../src/GalaxyInstance.inc';
 require_once '../src/Users.inc';
 require_once './testConfig.inc';
 
+
 class UsersTest extends PHPUnit_Framework_TestCase {
+	
+  
   /**
    *  Tests the index() function.
    *
@@ -19,11 +22,78 @@ class UsersTest extends PHPUnit_Framework_TestCase {
 
     // Create  Users object.
     $users = new Users($galaxy);
+	
+    $responses = $users->index();
 
-    // Case 1: Retrieve an array of all users
-    $user_list = $users->index();
-    print $users->getError();
-    print_r($user_list);
-
+    $contains_user = FALSE;
+   	foreach($responses as $response){
+   	  if(array_key_exists('username', $response)) {
+   	  	if($response['username'] == $config['username']){
+   	  	  $contains_user = TRUE;
+   	  	  break;
+   	  	}
+   	  }
+   	}    
+    // Case 1: Retrieve an array of all users	
+    $this->assertTrue($contains_user);
   }
+  
+  /**
+   * Test the show() function.
+   * 
+   * The show function retreives information on a specific user
+   */
+   function testShow(){
+   	global $config;
+   	
+   	// Connect to Galaxy.
+   	$galaxy = new GalaxyInstance($config['host'], $config['port'], FALSE);
+   	$response = $galaxy->authenticate($config['user'], $config['pass']);
+   	
+   	// Case 1: Retreive information about existing user 
+   	$users = new Users($galaxy); 
+   	
+   	$response = $users->show($config["user_id"]);
+
+   	$this->assertTrue(($response!= FALSE && count($response) > 0));
+   	
+   	//Case2: Wrong user id entered
+   	
+   	$response = $users->show("123456");
+   	$this->assertFalse(($response!= FALSE && count($response) > 0));   	
+   	
+   }
+   
+   /**
+    * Test the create() function.
+    * 
+    * The create function creates a new user
+    */
+   function testCreate(){
+   	global $config;
+   	
+   	// Connect to Galaxy.
+   	$galaxy = new GalaxyInstance($config['host'], $config['port'], FALSE);
+   	$response = $galaxy->authenticate($config['user'], $config['pass']);
+   	
+   	//Case 1, create a new user correctly
+   	$users = new Users($galaxy);
+   	
+   	$users->create("bimbo9000", "bimbo@yahoo.com", "password");
+   	$responses = $users->index();
+   	$contains_user = FALSE;
+   	foreach($responses as $response){
+   		if(array_key_exists('username', $response)) {
+   			if($response['username'] == "bimbo9000"){
+   				$contains_user = TRUE;
+   				break;
+   			}
+   		}
+   	}
+   	
+   	$this->assertTrue($contains_user);
+   	
+   	
+   }
+   
 }
