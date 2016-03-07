246102b7d5a826edc768d9dbcdf49c28411366dc
theBrian <kungfunub@gmail.com>
2016-03-07 09:23:32 -0800

    Updating

--- tests/UsersTest.php
+++ tests/UsersTest.php
@@ -14,21 +14,21 @@ class UsersTest extends PHPUnit_Framework_TestCase {
    *  The index function retrieves a list of users.
    */
   function testIndex() {
-    global $config_brian;
+    global $config;
 
     // Connect to Galaxy.
-    $galaxy = new GalaxyInstance($config_brian['host'], $config_brian['port'], FALSE);
-    $response = $galaxy->authenticate($config_brian['user'], $config_brian['pass']);
+    $galaxy = new GalaxyInstance($config['host'], $config['port'], FALSE);
+    $response = $galaxy->authenticate($config['user'], $config['pass']);
 
     // Create  Users object.
     $users = new Users($galaxy);
 	
     $responses = $users->index();
-
+	printf("\n \n THIS IS THE RESPONSE FROM USERS INDEX" );
     $contains_user = FALSE;
    	foreach($responses as $response){
    	  if(array_key_exists('username', $response)) {
-   	  	if($response['username'] == $config_brian['username']){
+   	  	if($response['username'] == $config['username']){
    	  	  $contains_user = TRUE;
    	  	  break;
    	  	}
@@ -44,16 +44,22 @@ class UsersTest extends PHPUnit_Framework_TestCase {
    * The show function retreives information on a specific user
    */
    function testShow(){
-   	global $config_brian;
+   	global $config;
+   	
    	
    	// Connect to Galaxy.
-   	$galaxy = new GalaxyInstance($config_brian['host'], $config_brian['port'], FALSE);
-   	$response = $galaxy->authenticate($config_brian['user'], $config_brian['pass']);
+   	$galaxy = new GalaxyInstance($config['host'], $config['port'], FALSE);
+   	$response = $galaxy->authenticate($config['user'], $config['pass']);
    	
    	// Case 1: Retreive information about existing user 
    	$users = new Users($galaxy); 
+   	$user_list = $users->index();
    	
-   	$response = $users->show($config_brian["user_id"]);
+   	foreach($user_list as $person){
+   		if(array_key_exists('username', $person) && ($person['username'] ))
+   	}
+   	
+   	$response = $users->show($config["user_id"]);
 
    	$this->assertTrue(($response!= FALSE && count($response) > 0));
    	
@@ -70,11 +76,11 @@ class UsersTest extends PHPUnit_Framework_TestCase {
     * The create function creates a new user
     */
    function testCreate(){
-   	global $config_brian;
+   	global $config;
    	
    	// Connect to Galaxy.
-   	$galaxy = new GalaxyInstance($config_brian['host'], $config_brian['port'], FALSE);
-   	$response = $galaxy->authenticate($config_brian['user'], $config_brian['pass']);
+   	$galaxy = new GalaxyInstance($config['host'], $config['port'], FALSE);
+   	$response = $galaxy->authenticate($config['user'], $config['pass']);
    	
    	//Case 1, create a new user correctly
    	$users = new Users($galaxy);
@@ -83,17 +89,76 @@ class UsersTest extends PHPUnit_Framework_TestCase {
    	$responses = $users->index();
    	$contains_user = FALSE;
    	foreach($responses as $response){
-   		if(array_key_exists('username', $response)) {
-   			if($response['username'] == "bimbo9000"){
-   				$contains_user = TRUE;
-   				break;
-   			}
+   	  if(array_key_exists('username', $response)) {
+   	    if($response['username'] == "bimbo9000"){
+   		  $contains_user = TRUE;
+   		  break;
+   		  }
    		}
    	}
-   	
+  
    	$this->assertTrue($contains_user);
    	
+   	//Case 2: make sure server does not create user if bad emailed entered
+   	$responses = $users->create("bimbo8000", "bimbo@nothing", "password");
+   	$contains_user = FALSE;
+   	if($responses != FALSE){	
+   	  foreach($responses as $response){
+   	    if(array_key_exists('username', $response)) {
+   	      if($response['username'] == "bimbo8000"){
+   		    $contains_user = TRUE;
+   		      break;
+   		  }
+   		}
+   	  }
+   	}
+   	$this->assertFalse($contains_user);
    	
    }
    
+   /**
+    * Test the delete function 
+    * 
+    * Deletes a user
+    */
+  // function testDelete(){
+ /**
+   	global $config;
+   	
+   	// Connect to Galaxy.
+   	$galaxy = new GalaxyInstance($config['host'], $config['port'], FALSE);
+   	$response = $galaxy->authenticate($config['user'], $config['pass']);
+   	
+   	//Case 1, successfully delete a user
+   	$users = new Users($galaxy);
+   	
+   	$response = $user->delete();
+   	*/
+  // }
+   
+   /**
+    * Test the api key function 
+    * 
+    * generates a new api key for a user
+    */
+   function testAPIKEY(){
+   	global $config;
+   	// Connect to Galaxy.
+   	$galaxy = new GalaxyInstance($config['host'], $config['port'], FALSE);
+   	$response = $galaxy->authenticate($config['user'], $config['pass']);
+   	
+   	//Case 1, enter an api key for a user
+   	$users = new Users($galaxy);
+   	
+   	$id = $user->
+   	
+   	$response = $users->api_key($config['user_id'] );
+   	print($users->getError);
+   	print("This is a response \n");
+   	var_dump($response);
+   	
+   	
+   }
+    
+
 }
