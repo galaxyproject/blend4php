71b8cf43810017cc9cccb27d541355f9c9256b2b
Stephen Ficklin <spficklin@gmail.com>
2016-03-07 22:27:00 -0800

    Added a phpunit.xml file containing a test suite of ordered tests

--- tests/RequestErrorTest.php
+++ tests/RequestErrorTest.php
@@ -12,56 +12,27 @@ require_once "../src/RequestError.inc";
 class RequestErrorTest extends PHPUnit_Framework_TestCase {
 
   /**
-   * Tests the parseCURLResponse function to see if it can catch all of the errors.
-   *
-   * Compose a dummy json like object for this function to play with, one with
-   * the right pieces and one without to make sure that this error handler does
-   * what it's supposed to.
-   *
-   * @param $message
-   *   The json object to parse to then be interpreted and presented to the user
-   *
+   * Tests the setError() and getError() functions.
    */
-  public function testparseCURLResponse(){
-  // Construct Dummy object
+  function testGetSet() {
+    // We simply need to make sure that the setter works.
+    $error = new RequestError();
+    $error->setError('HTTP', 'test_message');
+    $error = $error->getError();
 
-    // Test 1: test case #1 with message not in JSON format.
-    $dummy = 'asdaljfa;lskjfaf';
-    $handler = new RequestError();
-    $response = $handler->parseCURLResponse($dummy);
-    $this->assertFalse($response);
-    $error = $handler->getError();
-    $this->assertEquals('Galaxy', $error['type']);
-    $this->assertEquals('Unknown CURL response: not json compatible.', $error['message']);
+    // Case 1:  The return value should always be an array.
+    $this->assertTrue(is_array($error) , 'The RequestError return value is not an array: ' . print_r($error, TRUE));
 
-    // Test 2a:  tests case #2 with python failure with an error message.
-    $dummy = '{"traceback": "No traceback available.", "err_msg": "API authentication required for this request", "err_code": 403001}';
-    $handler = new RequestError();
-    $response = $handler->parseCURLResponse($dummy);
-    $this->assertFalse($response);
-    $error = $handler->getError();
-    $this->assertEquals('Galaxy', $error['type']);
-    $this->assertEquals('API authentication required for this request', $error['message']);
+    // Case 2:  Make sure the array is formatted properly:
+    $this->assertTrue(array_key_exists('type', $error), 'The RequestError is missing the "type" key: '. print_r($error, TRUE));
+    $this->assertTrue(array_key_exists('message', $error), 'The RequestError is missing the "message" key: '. print_r($error, TRUE));
 
-    // Test 2b:  test case #3 with a python failure but no error message.
-    $dummy = '{"traceback": "No traceback available.", "err_code": 403001}';
-    $response = $handler->parseCURLResponse($dummy);
-    $this->assertFalse($response);
-    $error = $handler->getError();
-    $this->assertEquals('Galaxy', $error['type']);
-    $this->assertEquals('A galaxy error was detected, though its contents are unkown', $error['message']);
+    // Case 3:  The type should match the type provided.
+    $this->assertEquals($error['type'], 'HTTP', 'The RequestError type does not match what was provided: '. print_r($error, TRUE));
 
-    // Test 3:  tests case # where galaxy generated the error.
-    // TODO: Find example to use for this test
-
-
-    // Test 4:  on a successful message do we get the proper array format
-    // with the correct keys.
-    $dummy = '[{ "id": "f2db41e1fa331b3e", "model_class": "Role", "name": "testuser@mail.com", "url": "/api/roles/f2db41e1fa331b3e"},  {"id": "f597429621d6eb2b", "model_class": "Role", "name": "blah@mail.com", "url": "/api/roles/f597429621d6eb2b"}]';
-    $response = $handler->parseCURLResponse($dummy);
-    $this->assertTrue(array_key_exists('id', $response[0]));
-    $this->assertEquals('f2db41e1fa331b3e', $response[0]['id']);
-
+    // Case 3:  The message should be teh message provided.
+    $this->assertEquals($error['message'], 'test_message', 'The RequestError message does not match what was provided: '. print_r($error, TRUE));
 
   }
+
 }
