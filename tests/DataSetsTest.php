5c3131ca6dbab07562330f55a724de619e109325
theBrian <kungfunub@gmail.com>
2016-03-08 16:43:59 -0800

    I have committed changes to datasets

--- tests/DataSetsTest.php
+++ tests/DataSetsTest.php
@@ -1,10 +1,30 @@
 <?php
 require_once '../src/DataSets.inc';
+require_once '../src/GalaxyInstance.inc';
 require_once './testConfig.inc';
-
+require_once './testConfig.inc';
 
 class DataSetsTest extends PHPUnit_Framework_TestCase {
 
+	/**
+	 * Intializes the Galaxy object for all of the tests.
+	 *
+	 * This function provides the $galaxy object to all other tests as they
+	 * are dependent on this one.
+	 * 
+	 * 
+	 */
+	function testInitGalaxy() {
+		global $config;
+	
+		// Connect to Galaxy.
+		$galaxy = new GalaxyInstance($config['host'], $config['port'], FALSE);
+		$success = $galaxy->authenticate($config['email'], $config['pass']);
+		$this->assertTrue($success, $galaxy->getErrorMessage());
+	
+		return $galaxy;
+	}	
+	
   /**
    *  Tests the index() function.
    *
@@ -12,6 +32,15 @@ class DataSetsTest extends PHPUnit_Framework_TestCase {
    */
   public function testIndex() {
     global $config;
+    
+    
+    
+    // Create  Visualization object.
+    $Datasets = new Datasets($galaxy);
+    
+    // Case 1:  Are we getting an array?
+    $response = $Datasets->index();
+    $this->assertTrue(is_array($response), $Datasets->getErrorMessage());
 
   }
 }
