3d4ab032500bdef1d189743f5b49adcdef2856cf
Stephen Ficklin <spficklin@gmail.com>
2016-09-25 01:49:07 -0700

    Debugging TravisCI

---
+++ examples/create_user_get_api_key.php
@@ -0,0 +1,34 @@
+<?php
+
+# Include the blend4php library.
+require_once('../galaxy.inc');
+
+# Get the incoming arguments.
+$hostname  = $argv[1];
+$port      = $argv[2];
+$use_https = $argv[3];
+$api_key   = $argv[4];
+$username  = $argv[5];
+$email     = $argv[6];
+$password  = $argv[7];
+
+# Instantiate the Galaxy object.
+$galaxy = new GalaxyInstance($hostname, $port, $use_https);
+$galaxy->setAPIKey($api_key);
+
+# Instantiate the User's API object.
+$usersAPI = new GalaxyUsers($galaxy);
+
+# Create the new User
+$user = $usersAPI->create(array(
+  'username' => $username,
+  'email' => $email,
+  'password' => $password,
+));
+
+# Set the API Key for this user. It is the 
+# current API associated with the galaxy object.
+$usersAPI->apiKey(array(
+  'user_id' => $user['id'],
+));
+
